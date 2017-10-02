615c0f2ecc9e8059fe89bd6d1b7cade49412f79f
Moustikitos <bruno.thoorens@free.fr>
2017-10-02 20:20:23 +0200

    Starting from scratch

--- arky-ui.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-# created by Toons on 25/07/2017
-
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(sys.executable), "site-packages.zip"))
-
-from arky import ui
-ui.launch()
