67db4883d58887436fb0a2814f137ce726d957c8
Moustikitos <bruno.thoorens@free.fr>
2017-10-08 21:52:27 +0200

    Github dist fix

--- test/test_core.py
+++
@@ -1,9 +0,0 @@
-from arky import __PY3__
-import doctest, binascii
-import arky.core
-
-if not __PY3__:
-	raise Exception("doctest is written for python 3.x familly")
-
-arky.core.api.use("testnet")
-doctest.testmod(arky.core)
