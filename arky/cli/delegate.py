0baee5c14dee4095dbdaee8f1f16d03a5d9eda1c
jolan <mc-somango@live.fr>
2017-10-10 00:09:08 +0200

    Fixedline 167 where C_max was undefined and needed to be max_C instead. Added space between the functions for PEP8 guidelines

--- arky/cli/delegate.py
+++ arky/cli/delegate.py
@@ -1,13 +1,13 @@
 # -*- encoding: utf8 -*-
 # © Toons
 
-'''
+"""
 Usage: delegate link [<secret> <2ndSecret>]
-       delegate save <name>
-       delegate unlink
-       delegate status
-       delegate voters
-       delegate share <amount> [-c -b <blacklist> -d <delay> -l <lowest> -h <highest> <message>]
+	   delegate save <name>
+	   delegate unlink
+	   delegate status
+	   delegate voters
+	   delegate share <amount> [-c -b <blacklist> -d <delay> -l <lowest> -h <highest> <message>]
 
 Options:
 -b <blacklist> --blacklist <blacklist> ark addresses to exclude (comma-separated list or pathfile)
@@ -17,17 +17,17 @@ Options:
 -c --complement                        share the amount complement
 
 Subcommands:
-    link   : link to delegate using secret passphrases. If secret passphrases
-             contains spaces, it must be enclosed within double quotes
-             ("secret with spaces"). If no secret given, it tries to link
-             with saved account(s).
-    save   : save linked delegate to a *.tokd file.
-    unlink : unlink delegate.
-    status : show information about linked delegate.
-    voters : show voters contributions ([address - vote] pairs).
-    share  : share ARK amount with voters (if any) according to their
-             weight (1% mandatory fees). You can set a 64-char message. 
-'''
+	link   : link to delegate using secret passphrases. If secret passphrases
+			 contains spaces, it must be enclosed within double quotes
+			 ("secret with spaces"). If no secret given, it tries to link
+			 with saved account(s).
+	save   : save linked delegate to a *.tokd file.
+	unlink : unlink delegate.
+	status : show information about linked delegate.
+	voters : show voters contributions ([address - vote] pairs).
+	share  : share ARK amount with voters (if any) according to their
+			 weight (1% mandatory fees). You can set a 64-char message.
+"""
 
 from .. import cfg, api, core, ROOT
 from .. util import stats
@@ -45,12 +45,13 @@ DELEGATE = None
 
 try:
 	version_info = sys.version_info[:2]
-	if version_info == (2, 7):   from . import pshare27 as pshare
+	if version_info == (2, 7): from . import pshare27 as pshare
 	elif version_info == (3, 5): from . import pshare35 as pshare
 	elif version_info == (3, 6): from . import pshare36 as pshare
 	SHARE = True
 except ImportError:
 	SHARE = False
+
 
 def link(param):
 	global ADDRESS, PUBLICKEY, KEY1, KEY2, USERNAME, DELEGATE
@@ -82,18 +83,22 @@ def link(param):
 	if ADDRESS:
 		common.BALANCES.register(ADDRESS)
 
+
 def save(param):
 	if KEY1 and PUBLICKEY and ADDRESS:
 		common.dropToken(common.tokenPath(param["<name>"], "tokd"), ADDRESS, PUBLICKEY, KEY1)
+
 
 def unlink(param):
 	global ADDRESS, PUBLICKEY, KEY1, KEY2, USERNAME, DELEGATE
 	common.BALANCES.pop(ADDRESS, None)
 	ADDRESS, PUBLICKEY, KEY1, KEY2, USERNAME, DELEGATE = None, None, None, None, None, None
 
+
 def status(param):
 	if ADDRESS:
 		common.prettyPrint(dict(DELEGATE, **api.Account.getAccount(ADDRESS, returnKey="account")))
+
 
 def voters(param):
 	if PUBLICKEY:
@@ -105,6 +110,7 @@ def voters(param):
 			sum_ += vote
 		sys.stdout.write("    " + "-"*(len(line)-5) + "\n")
 		sys.stdout.write("    %s: %.3f\n" % (("%d voters"%len(accounts)).rjust(len(addr)), sum_))
+
 
 def share(param):
 	global KEY1, KEY2, ADDRESS
@@ -158,7 +164,7 @@ def share(param):
 						i += 1
 						n -= 1
 						contribution[address] = max_C
-						cumul -= abs(C_max - force)
+						cumul -= abs(max_C - force)
 						bounty = cumul / n
 					else:
 						break
@@ -174,12 +180,14 @@ def share(param):
 		else:
 			sys.stdout.write("Share feature not available\n")
 
+
 # --------------
 def _whereami():
 	if USERNAME:
 		return "delegate[%s]" % USERNAME
 	else:
 		return "delegate"
+
 
 def _checkIfDelegate():
 	global DELEGATE
