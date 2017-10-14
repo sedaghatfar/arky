904209816eb83222cb99324184d3422aa0b51e8b
jolan <mc-somango@live.fr>
2017-10-14 23:46:20 +0200

    Some basic testing for the api, nothing fancy right now.

--- test/test_api.py
+++ test/test_api.py
@@ -1,5 +1,18 @@
 import unittest
-from arky.api import Block
+from arky.api import Block, Account, get
+import requests
+import json
+from arky import ArkyDict
+
+def getBis(api, **params):
+    url = "https://api.arknode.net/{0}".format(api)
+    payload = {
+        "port": 4001,
+        "version": "1.0.1",
+        "nethash": "6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988"
+    }
+    r = ArkyDict(json.loads(requests.get(url, params=payload).text))
+    return r
 
 
 class TestBlockAPI(unittest.TestCase):
@@ -50,5 +63,13 @@ class TestBlockAPI(unittest.TestCase):
     #    print(Block.getForgedByAccount("038e82be3e92018374c9b53a25a428799602366635d5270c5154d945ac60216ffe", **params))
 
 
+class TestAccountAPI(unittest.TestCase):
+    def test_getDelegates(self):
+        address = "Acxb4Wxt2oUsXVViHJsenMuRDUjsMHHKeM"
+        req = getBis("/api/accounts/delegates", address)
+        self.assertEqual(req['success'], True)
+
+
 if __name__ == '__main__':
     unittest.main()
+    #print(getBis("Acxb4Wxt2oUsXVViHJsenMuRDUjsMHHKeM"))
