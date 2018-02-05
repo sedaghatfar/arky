63613625c5dda9f729c6a7018123f05dc86459f2
roks0n <haluzan.rok@gmail.com>
2018-02-05 19:57:57 +0100

    Modifications to rest.py, ark/lisk init + test rest.py + flake8 + modify requirements.txt

--- arky/ark/__init__.py
+++ arky/ark/__init__.py
@@ -1,144 +1,170 @@
 # -*- encoding: utf8 -*-
 # © Toons
+import logging
 
-from .. import __PY3__
-from .. import util
-from .. import rest
-from .. import cfg
-from .. import util
+from arky import cfg, rest, util
+from arky.ark import crypto
 
-from . import crypto
-
-import sys
-import random
-import threading
+log = logging.getLogger(__name__)
+DAEMON_PEERS = None
 
 
-def selectPeers():
-	version = rest.GET.api.peers.version().get("version", "0.0.0")
-	height = rest.GET.api.blocks.getHeight().get("height", 0)
+def select_peers():
+    version = rest.GET.api.peers.version(returnKey='version') or '0.0.0'
+    height = rest.GET.api.blocks.getHeight(returnKey='height') or 0
 
-	peers = rest.GET.peer.list().get("peers", [])
-	good_peers = []
-	for peer in peers:
-		if (
-			peer.get("delay", 6000) <= cfg.timeout * 1000 and peer.get("version") == version and
-			peer.get("height", -1) > height - 10
-		):
-			good_peers.append(peer)
+    peers = rest.GET.peer.list().get('peers', [])
+    good_peers = []
+    for peer in peers:
+        if (
+            peer.get("delay", 6000) <= cfg.timeout * 1000 and peer.get("version") == version and
+            peer.get("height", -1) > height - 10
+        ):
+            good_peers.append(peer)
 
-	good_peers = sorted(good_peers, key=lambda e: e["delay"])
+    good_peers = sorted(good_peers, key=lambda e: e["delay"])
 
-	selection = []
-	while(len(selection) < min(getattr(cfg, "broadcast", 0), len(good_peers))):
-		peer = "http://{ip}:{port}".format(**good_peers[len(selection)])
-		if rest.checkPeerLatency(peer):
-			selection.append(peer)
+    min_selection = getattr(cfg, "broadcast", 0)
+    selection = []
+    for peer in good_peers:
+        peer = "http://{ip}:{port}".format(**peer)
+        if rest.check_latency(peer):
+            selection.append(peer)
 
-	if len(selection):
-		cfg.peers = selection
+        if len(selection) >= min_selection:
+            break
+
+    if len(selection) < min_selection:
+        log.debug(
+            'Broadcast it set to "%s", but managed to get %s peers out of %s.',
+            min_selection, len(selection), len(peers)
+        )
+
+    if selection:
+        cfg.peers = selection
+    else:
+        raise Exception('Was not able to get even one live peer out of %s peers.\n' % len(peers))
+
+
+@util.setInterval(30)
+def rotate_peers():
+    select_peers()
 
 
 def init():
-	global DAEMON_PEERS
-	resp = rest.GET.api.loader.autoconfigure()
-	if resp["success"]:
-		network = resp["network"]
-		cfg.headers["version"] = "%s" % network.pop("version")
-		cfg.headers["nethash"] = network.pop("nethash")
-		cfg.__dict__.update(network)
-		cfg.fees = rest.GET.api.blocks.getFees().get("fees")
-		# manage peers for tx broadcasting
-		threading.Thread(target=selectPeers).start()
-		@util.setInterval(30)
-		def rotatePeers(): selectPeers()
-		DAEMON_PEERS = rotatePeers()
-	else:
-		sys.stdout.write(("%s\n" % resp.get("error", "...")).encode("ascii", errors="replace").decode())
-		raise Exception("Initialization error with peer %s" % resp.get("peer", "???"))
+    global DAEMON_PEERS
+    response = rest.GET.api.loader.autoconfigure()
+    if response["success"]:
+        network = response["network"]
+        cfg.headers["version"] = str(network.pop('version'))
+        cfg.headers["nethash"] = network.pop('nethash')
+
+        cfg.__dict__.update(network)
+        cfg.fees = rest.GET.api.blocks.getFees(returnKey="fees")
+
+        # select peers immediately and keep refreshing them in a thread so we
+        # are sure we make requests to working peers
+        select_peers()
+        DAEMON_PEERS = rotate_peers()
+    else:
+        log.error(response.get('error', '...'))
+        raise Exception("Initialization error with peer %s" % response.get("peer", "???"))
 
 
 def sendPayload(*payloads):
-	success, msgs, ids = 0, set([]), set([])
+    success, msgs, ids = 0, set(), set()
 
-	for peer in cfg.peers:
-		resp = rest.POST.peer.transactions(peer=peer, transactions=payloads)
-		success += 1 if resp["success"] else 0
-		if "message" in resp: msgs.update([resp["message"]])
-		if "transactionIds" in resp: ids.update(resp["transactionIds"])
+    for peer in cfg.peers:
+        response = rest.POST.peer.transactions(peer=peer, transactions=payloads)
+        success += 1 if response["success"] else 0
 
-	return {
-		"success": "%.1f%%" % (100.*success/len(cfg.peers)),
-		"transactions": list(ids),
-		"messages": list(msgs)
-	}
+        if "message" in response:
+            msgs.update([response["message"]])
+
+        if "transactionIds" in response:
+            ids.update(response["transactionIds"])
+
+    return {
+        "success": "%.1f%%" % (float(100) * success / len(cfg.peers)),
+        "transactions": list(ids),
+        "messages": list(msgs)
+    }
 
 
 # This function is a high-level broadcasting for a single tx
-def sendTransaction(**kw):
-	tx = crypto.bakeTransaction(**dict([k,v] for k,v in kw.items() if v))
-	return sendPayload(tx)
+def sendTransaction(**kwargs):
+    tx = crypto.bakeTransaction(**dict([k, v] for k, v in kwargs.items() if v))
+    return sendPayload(tx)
 
 
 #######################
-## basic transaction ##
+#  basic transaction  #
 #######################
 
 def sendToken(amount, recipientId, secret, secondSecret=None, vendorField=None):
-	return sendTransaction(
-		amount=amount,
-		recipientId=recipientId,
-		vendorField=vendorField,
-		secret=secret,
-		secondSecret=secondSecret
-	)
+    return sendTransaction(
+        amount=amount,
+        recipientId=recipientId,
+        vendorField=vendorField,
+        secret=secret,
+        secondSecret=secondSecret
+    )
+
 
 def registerSecondPublicKey(secondPublicKey, secret, secondSecret=None):
-	keys = crypto.getKeys(secret)
-	return sendTransaction(
-		type=1,
-		publicKey=keys["publicKey"],
-		privateKey=keys["privateKey"],
-		secondSecret=secondSecret,
-		asset={"signature":{"publicKey":secondPublicKey}}
-	)
+    keys = crypto.getKeys(secret)
+    return sendTransaction(
+        type=1,
+        publicKey=keys["publicKey"],
+        privateKey=keys["privateKey"],
+        secondSecret=secondSecret,
+        asset={"signature": {"publicKey": secondPublicKey}}
+    )
+
 
 def registerSecondPassphrase(secondPassphrase, secret, secondSecret=None):
-	secondKeys = crypto.getKeys(secondPassphrase)
-	return registerSecondPublicKey(secondKeys["publicKey"], secret, secondSecret)
+    secondKeys = crypto.getKeys(secondPassphrase)
+    return registerSecondPublicKey(secondKeys["publicKey"], secret, secondSecret)
+
 
 def registerDelegate(username, secret, secondSecret=None):
-	keys = crypto.getKeys(secret)
-	return sendTransaction(
-		type=2,
-		publicKey=keys["publicKey"],
-		privateKey=keys["privateKey"],
-		secondSecret=secondSecret,
-		asset={"delegate":{"username":username, "publicKey":keys["publicKey"]}}
-	)
+    keys = crypto.getKeys(secret)
+    return sendTransaction(
+        type=2,
+        publicKey=keys["publicKey"],
+        privateKey=keys["privateKey"],
+        secondSecret=secondSecret,
+        asset={
+            "delegate": {
+                "username": username, "publicKey": keys["publicKey"]
+            }
+        }
+    )
+
 
 def upVoteDelegate(usernames, secret, secondSecret=None):
-	keys = crypto.getKeys(secret)
-	req = rest.GET.api.delegates.get(username=usernames[-1])
-	if req["success"]:
-		return sendTransaction(
-			type=3,
-			publicKey=keys["publicKey"],
-			recipientId=crypto.getAddress(keys["publicKey"]),
-			privateKey=keys["privateKey"],
-			secondSecret=secondSecret,
-			asset={"votes":["+%s"%req["delegate"]["publicKey"]]}
-		)
+    keys = crypto.getKeys(secret)
+    req = rest.GET.api.delegates.get(username=usernames[-1])
+    if req["success"]:
+        return sendTransaction(
+            type=3,
+            publicKey=keys["publicKey"],
+            recipientId=crypto.getAddress(keys["publicKey"]),
+            privateKey=keys["privateKey"],
+            secondSecret=secondSecret,
+            asset={"votes": ["+%s" % req["delegate"]["publicKey"]]}
+        )
+
 
 def downVoteDelegate(usernames, secret, secondSecret=None):
-	keys = crypto.getKeys(secret)
-	req = rest.GET.api.delegates.get(username=usernames[-1])
-	if req["success"]:
-		return sendTransaction(
-			type=3,
-			publicKey=keys["publicKey"],
-			recipientId=crypto.getAddress(keys["publicKey"]),
-			privateKey=keys["privateKey"],
-			secondSecret=secondSecret,
-			asset={"votes":["-%s"%req["delegate"]["publicKey"]]}
-		)
+    keys = crypto.getKeys(secret)
+    req = rest.GET.api.delegates.get(username=usernames[-1])
+    if req["success"]:
+        return sendTransaction(
+            type=3,
+            publicKey=keys["publicKey"],
+            recipientId=crypto.getAddress(keys["publicKey"]),
+            privateKey=keys["privateKey"],
+            secondSecret=secondSecret,
+            asset={"votes": ["-%s" % req["delegate"]["publicKey"]]}
+        )
