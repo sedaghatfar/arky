920a1d880e371edf153ebd6e58dabaad31265877
roks0n <haluzan.rok@gmail.com>
2018-03-16 20:52:16 +0100

    Fix and write tests for: prettyfy, prettyPrint, getBytes and json fns

--- test/test_util.py
+++ test/test_util.py
@@ -1,9 +1,10 @@
 # -*- encoding: utf8 -*-
-# © Toons
-
+import os
+import io
 import unittest
-
-from arky import rest, cli
+from collections import OrderedDict
+from mock import patch
+from arky import rest, util
 
 
 class TestUtilDef(unittest.TestCase):
@@ -20,6 +21,29 @@ class TestUtilDef(unittest.TestCase):
 		# can' test because `floatAmount` function still seems to be WIP
 		pass
 
+	def test_json(self):
+		"""
+		When calling `dumpJson` a file with a json dump should be created and we should be able to
+		get its contents with `loadJson`. Our `popJson` fn should remove the file.
+		"""
+		data = {"testing": True}
+		filename = "test.txt"
+		util.dumpJson(data, filename)
+		data = util.loadJson(filename)
+		assert data["testing"] is True
+
+		# test if file will be removed
+		util.popJson(filename)
+		assert not os.path.exists(filename)
+
+	def test_prettyPrint(self):
+		# we use OrderedDict to always guarantee the same order as a result so we can assert it
+		data = OrderedDict()
+		data["testing"] = True
+		data["transactions"] = "many"
+		with patch('sys.stdout', new=io.StringIO()) as stdout:
+			util.prettyPrint(data, log=False)
+		assert stdout.getvalue() == '\t     testing: True\n\ttransactions: many\n'
 
 if __name__ == '__main__':
     unittest.main()
