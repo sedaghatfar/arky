6eed2c663fdc9e630493b2a98aa0adbe57b2c18a
roks0n <haluzan.rok@gmail.com>
2018-03-18 10:11:15 +0100

    write tests for chooseItem and chooseMultipleItem

--- test/utils/test_cli.py
+++ test/utils/test_cli.py
@@ -1,11 +1,12 @@
 # -*- encoding: utf8 -*-
 import io
+
 import unittest
 
 from collections import OrderedDict
 from mock import patch
 
-from arky.utils.cli import prettyPrint, shortAddress
+from arky.utils.cli import prettyPrint, shortAddress, chooseItem, chooseMultipleItem
 
 
 class TestUtilsCLI(unittest.TestCase):
@@ -23,6 +24,21 @@ class TestUtilsCLI(unittest.TestCase):
         output = shortAddress("DUGvQBxLzQqrNy68asPcU3stWQyzVq8G49")
         assert output == "DUGvQ...q8G49"
 
+    @patch('arky.utils.cli.input', return_value="2")
+    def test_chooseItem(self, input):
+        output = chooseItem("Network(s) found:", *["ark", "dark", "lisk"])
+        assert output == "dark"
+
+    def test_chooseMultipleItem(self):
+        scenarios = [
+            ("1, 2", [1, 2]),
+            ("all", [1, 2, 3])
+        ]
+        for input_value, expected in scenarios:
+            with patch('arky.utils.cli.input') as mocked_input:
+                mocked_input.return_value = input_value
+                output = chooseMultipleItem("Transactions(s) found:", *["first", "second", "third"])
+            assert output == expected
 
 if __name__ == '__main__':
     unittest.main()
