4b1b594a2ff68d28b733d27dd0ba7cfa482ceb67
Moustikitos <bruno.thoorens@free.fr>
2018-04-10 22:35:32 +0200

    lisk crypto fix tpersona network added

--- arky/utils/bin.py
+++ arky/utils/bin.py
@@ -4,6 +4,21 @@ import struct
 from six import PY3
 
 
+# # byte as int conversion
+# basint = (lambda e:e) if PY3 else \
+#          (lambda e:ord(e))
+# # read value as binary data from buffer
+# unpack =  lambda fmt, fileobj: struct.unpack(fmt, fileobj.read(struct.calcsize(fmt)))
+# # write value as binary data into buffer
+# pack = lambda fmt, fileobj, value: fileobj.write(struct.pack(fmt, *value))
+# # read bytes from buffer
+# unpack_bytes = lambda f,n: unpack("<"+"%ss"%n, f)[0]
+# # write bytes into buffer
+# pack_bytes = (lambda f,v: pack("!"+"%ss"%len(v), f, (v,))) if PY3 else \
+#              (lambda f,v: pack("!"+"c"*len(v), f, v))
+
+
+
 def basint(e):
     # byte as int conversion
     if not PY3:
